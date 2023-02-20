a7ca8d52a8b09dda68c305116c7213a2481097ac
bertrandmd <bertrand.md@gmail.com>
2023-02-20 14:24:11 +0100

    feat: add example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,58 +4,29 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter_wemap_sdk/flutter_wemap_sdk.dart';
 
+import 'map_view.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _flutterWemapSdkPlugin = FlutterWemapSdk();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _flutterWemapSdkPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+class MyApp extends StatelessWidget {
+  final _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scaffoldMessengerKey: _scaffoldKey,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text("Wemap Sample"),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Container(
+          constraints: const BoxConstraints.expand(),
+          child: MapView(
+            scaffoldMessengerKey: _scaffoldKey,
+          ),
         ),
       ),
     );
---
+++ example/lib/map_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wemap_sdk/flutter_wemap_sdk.dart';
+import 'package:flutter_wemap_sdk/livemap_controller.dart';
+
+class MapView extends StatelessWidget {
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey;
+  const MapView({super.key, required this.scaffoldMessengerKey});
+
+  void onNativeMapReady() {
+    const snackBar = SnackBar(content: Text('Map is Ready'));
+    scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+  }
+
+  void onPinpointOpen(dynamic pinpoint) {
+    var snackBar = SnackBar(content: Text('Open Pinpoint: ${pinpoint.name}'));
+
+    scaffoldMessengerKey.currentState?.clearSnackBars();
+    scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+  }
+
+  void onPinpointClose() {
+    const snackBar = SnackBar(content: Text('Close Pinpoint'));
+
+    scaffoldMessengerKey.currentState?.clearSnackBars();
+    scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+  }
+
+  void onContentUpdated(List<dynamic> pinpoints) {
+    var snackBar =
+        SnackBar(content: Text('Updated Pinpoints count: ${pinpoints.length}'));
+    scaffoldMessengerKey.currentState?.clearSnackBars();
+    scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+  }
+
+  // bind controller
+  void _onMapCreated(LivemapController mapController) {
+    const snackBar = SnackBar(content: Text('Livemap created'));
+    scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+    // whatever with mapController
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const Map<String, dynamic> creationParams = <String, dynamic>{
+      "token": "GUHTU6TYAWWQHUSR5Z5JZNMXX",
+      "emmid": 21262
+    };
+
+    return Livemap(
+      options: creationParams,
+      onMapCreated: _onMapCreated,
+      onMapReady: onNativeMapReady,
+      onPinpointOpen: onPinpointOpen,
+      onPinpointClose: onPinpointClose,
+      onContentUpdated: onContentUpdated,
+    );
+  }
+}
