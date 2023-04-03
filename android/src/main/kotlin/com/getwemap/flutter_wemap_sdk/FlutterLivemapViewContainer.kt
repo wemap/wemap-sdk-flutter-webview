634bda332414cfc4b85dca3a962c13882f7a21fb
Mustapha Elo <randy.mostafa61@gmail.com>
2023-04-03 10:23:38 +0200

    feat(methods): handle missing methods from sdk

--- android/src/main/kotlin/com/getwemap/flutter_wemap_sdk/FlutterLivemapViewContainer.kt
+++ android/src/main/kotlin/com/getwemap/flutter_wemap_sdk/FlutterLivemapViewContainer.kt
@@ -7,23 +7,26 @@ import android.view.View
 import androidx.annotation.NonNull
 import com.getwemap.livemap.sdk.Livemap
 import com.getwemap.livemap.sdk.LivemapView
+import com.getwemap.livemap.sdk.callback.FindNearestPinpointsCallback
+import com.getwemap.livemap.sdk.callback.GetZoomCallback
 import com.getwemap.livemap.sdk.callback.LivemapReadyCallback
 import com.getwemap.livemap.sdk.listener.ContentUpdatedListener
 import com.getwemap.livemap.sdk.listener.PinpointCloseListener
 import com.getwemap.livemap.sdk.listener.PinpointOpenListener
-import com.getwemap.livemap.sdk.model.Coordinates
-import com.getwemap.livemap.sdk.model.Event
-import com.getwemap.livemap.sdk.model.Pinpoint
-import com.getwemap.livemap.sdk.model.Query
+import com.getwemap.livemap.sdk.listener.UserLoginListener
+import com.getwemap.livemap.sdk.model.*
 import com.getwemap.livemap.sdk.options.LivemapOptions
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
 import com.google.gson.ToNumberPolicy
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.platform.PlatformView
+import org.json.JSONArray
+import org.json.JSONObject
 
 val gson: Gson = GsonBuilder()
     .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
@@ -38,7 +41,10 @@ class FlutterLivemapViewContainer(context: Context,
     LivemapReadyCallback,
     PinpointOpenListener,
     PinpointCloseListener,
-    ContentUpdatedListener {
+    ContentUpdatedListener,
+
+    UserLoginListener
+{
 
     private var view: View
     private var channel: MethodChannel
@@ -83,12 +89,15 @@ class FlutterLivemapViewContainer(context: Context,
     override fun onPinpointsUpdated(p0: Query?, pinpoints: MutableList<Pinpoint>?) {
         val pps = pinpoints?.map {gson.fromJson(it.toJson().toString(), HashMap<String, Object>().javaClass) }
         uiThreadHandler.post {
-            channel.invokeMethod("onContentUpdated", pps)
+            channel.invokeMethod("onPinpointUpdated", pps)
         }
     }
 
-    override fun onEventsUpdated(p0: Query?, p1: MutableList<Event>?) {
-        TODO("Not yet implemented")
+    override fun onEventsUpdated(p0: Query?, events: MutableList<Event>?) {
+        val evts = events?.map {gson.fromJson(it.toJson().toString(), HashMap<String, Object>().javaClass) }
+        uiThreadHandler.post {
+            channel.invokeMethod("onEventUpdated", evts)
+        }
     }
 
      override fun onLivemapReady(@NonNull livemap: Livemap) {
@@ -98,9 +107,17 @@ class FlutterLivemapViewContainer(context: Context,
          livemap.addPinpointCloseListener(this);
          livemap.addContentUpdatedListener(this);
 
+         livemap.addUserLoginListener(this);
+
          uiThreadHandler.post {
              sendOnMapReady()
          }
+    }
+
+    override fun onUserLogin() {
+        uiThreadHandler.post {
+            channel.invokeMethod("onUserLogin", null)
+        }
     }
 
     override fun onMethodCall(methodCall: MethodCall, result: MethodChannel.Result) {
@@ -109,6 +126,32 @@ class FlutterLivemapViewContainer(context: Context,
             "closePinpoint" -> closePinpoint(methodCall, result)
             "setCenter" -> setCenter(methodCall, result)
             "centerTo" -> centerTo(methodCall, result)
+            "openEvent" -> openEvent(methodCall, result)
+            "closeEvent" -> closeEvent(methodCall, result)
+            "openList" -> openList(methodCall, result)
+            "closeList" -> closeList(methodCall, result)
+            "closePopin" -> closePopin(methodCall, result)
+            "setFilters" -> setFilters(methodCall, result)
+            "navigateToPinpoint" -> navigateToPinpoint(methodCall, result)
+            "stopNavigation" -> stopNavigation(methodCall, result)
+            "signInByToken" -> signInByToken(methodCall, result)
+            "enableSidebar" -> enableSidebar(methodCall, result)
+            "disableSidebar" -> disableSidebar(methodCall, result)
+            "signOut" -> signOut(methodCall, result)
+            "setSourceLists" -> setSourceLists(methodCall, result)
+            "setPinpoints" -> setPinpoints(methodCall, result)
+            "setEvents" -> setEvents(methodCall, result)
+            "aroundMe" -> aroundMe(methodCall, result)
+            "enableAnalytics" -> enableAnalytics(methodCall, result)
+            "disableAnalytics" -> disableAnalytics(methodCall, result)
+            "drawPolyline" -> drawPolyline(methodCall, result)
+            "removePolyline" -> removePolyline(methodCall, result)
+            "addMarker" -> addMarker(methodCall, result)
+            "removeMarker" -> removeMarker(methodCall, result)
+            "findNearestPinpoints" -> findNearestPinpoints(methodCall, result)
+
+            "getZoom" -> getZoom(methodCall, result)
+
             else -> result.notImplemented()
         }
     }
@@ -147,7 +190,190 @@ class FlutterLivemapViewContainer(context: Context,
         // result.success(null)
     }
 
+    private fun openEvent(methodCall: MethodCall, result: MethodChannel.Result){
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val eventId: Int = params.get("event") as Int
+        livemap.openEvent(eventId)
+        // result.success(null)
+    }
+
+    private fun closeEvent(methodCall: MethodCall, result: MethodChannel.Result) {
+        livemap.closeEvent()
+        // result.success(null)
+    }
+
+    private fun openList(methodCall: MethodCall, result: MethodChannel.Result){
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val listId: Int = params.get("list") as Int
+        livemap.openList(listId)
+        // result.success(null)
+    }
+
+    private fun closeList(methodCall: MethodCall, result: MethodChannel.Result) {
+        livemap.closeList()
+        // result.success(null)
+    }
+
+    private fun closePopin(methodCall: MethodCall, result: MethodChannel.Result) {
+        livemap.closePopin()
+        // result.success(null)
+    }
+
+    private fun setFilters(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val filters: HashMap<String, Any> = params.get("filters") as HashMap<String, Any>
+        val flts = Filters(
+            filters.get("startDate") as String, filters.get("endDate") as String ,
+            filters.get("query") as String, filters.get("tags") as Array<String>)
+        livemap.setFilters(flts)
+        // result.success(null)
+    }
+
+    private fun navigateToPinpoint(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val pinpointId: Int = params.get("pinpoint") as Int
+        livemap.navigateToPinpoint(pinpointId)
+        // result.success(null)
+    }
+
+    private fun stopNavigation(methodCall: MethodCall, result: MethodChannel.Result) {
+        livemap.stopNavigation()
+        // result.success(null)
+    }
+
+    private fun signInByToken(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val accessToken: String = params.get("accessToken") as String
+        livemap.signInByToken(accessToken)
+        // result.success(null)
+    }
+
+    private fun enableSidebar(methodCall: MethodCall, result: MethodChannel.Result) {
+        livemap.enableSidebar()
+        // result.success(null)
+    }
+
+    private fun disableSidebar(methodCall: MethodCall, result: MethodChannel.Result) {
+        livemap.disableSidebar()
+        // result.success(null)
+    }
+
+    private fun signOut(methodCall: MethodCall, result: MethodChannel.Result) {
+        livemap.signOut()
+        // result.success(null)
+    }
+
+    private fun setSourceLists(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val sourceLists: List<Int> = params.get("sourceLists") as List<Int>
+        livemap.setSourceLists(sourceLists)
+        // result.success(null)
+    }
+
+    private fun setPinpoints(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val pinpoints: List<HashMap<String, Any>> = params.get("setPinpoints") as List<HashMap<String, Any>>
+        val pnpts: List<Pinpoint> = pinpoints.map {
+            val point: HashMap<String, Double> = it["coordinates"] as HashMap<String, Double>
+            val coords = Coordinates(point.get("latitude")!!, point.get("longitude")!!)
+            Pinpoint(it["id"] as Int, it["name"] as String, coords )
+        }
+        livemap.setPinpoints(pnpts)
+        // result.success(null)
+    }
+
+    private fun setEvents(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val events: List<HashMap<String, Any>> = params.get("events") as List<HashMap<String, Any>>
+        val evts: List<Event> = events.map {event ->
+            val pinpointMap: HashMap<String, Any> = event["pinpoint"] as HashMap<String, Any>
+            val coordsMap: HashMap<String, Double> = pinpointMap["coordinates"] as HashMap<String, Double>
+            val coords = Coordinates(coordsMap.get("latitude")!!, coordsMap.get("longitude")!!)
+            val pinpoint = Pinpoint(pinpointMap["id"] as Int, pinpointMap["name"] as String, coords )
+            val dates: Array<HashMap<String, String>> = event["dates"] as Array<HashMap<String, String>>
+            val eventDateArray: Array<EventDate> = dates.map { eventDate ->
+                EventDate(eventDate["start"] as String, eventDate["end"] as String)
+            }.toTypedArray()
+            Event(event["id"] as Int, event["name"] as String, pinpoint, eventDateArray)
+        }
+        livemap.setEvents(evts)
+        // result.success(null)
+    }
+
+    private fun aroundMe(methodCall: MethodCall, result: MethodChannel.Result) {
+            livemap.aroundMe()
+            // result.success(null)
+        }
+
+    private fun enableAnalytics(methodCall: MethodCall, result: MethodChannel.Result) {
+            livemap.enableAnalytics()
+            // result.success(null)
+        }
+
+    private fun disableAnalytics(methodCall: MethodCall, result: MethodChannel.Result) {
+            livemap.disableAnalytics()
+            // result.success(null)
+        }
+
+    private fun drawPolyline(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val coordinates: List<HashMap<String, Double>> = params.get("coordinates") as List<HashMap<String, Double>>
+
+        val coordsList: List<Coordinates> = coordinates.map {
+            val jsonObject = JSONObject(it as Map<*, *>)
+            Coordinates.fromJson(jsonObject)
+        }
+        livemap.drawPolyline(coordsList)
+        // result.success(null)
+    }
+
+    private fun removePolyline(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val polylineId: String = params.get("polylineId") as String
+        livemap.removePolyline(polylineId)
+            // result.success(null)
+        }
+
+    private fun addMarker(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val markerMap: HashMap<String, Any> = params["marker"] as HashMap<String, Any>
+        val coordinatesMap: HashMap<String, Double> = markerMap["coordinates"] as HashMap<String, Double>
+        val coords:Coordinates =  Coordinates(coordinatesMap["latitude"]!!, coordinatesMap["longitude"]!!)
+        val marker:Marker = Marker(coords , markerMap["image"] as String)
+        livemap.addMarker(marker)
+        // result.success(null)
+    }
+
+    private fun removeMarker(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val markerId: String = params["markerId"] as String
+        livemap.removeMarker(markerId)
+        // result.success(null)
+    }
+
+    private fun findNearestPinpoints(methodCall: MethodCall, result: MethodChannel.Result) {
+        val params: HashMap<String, Any> = methodCall.arguments as HashMap<String, Any>
+        val center: HashMap<String, Double> = params["center"] as HashMap<String, Double>
+        val jsonObject = JSONObject(center as Map<*, *>)
+
+        livemap.findNearestPinpoints(Coordinates.fromJson(jsonObject), FindNearestPinpointsCallback{pinpoints ->
+            val jsonObject = JSONObject(pinpoints.toString())
+            result.success(pinpoints.toString());
+        })
+    }
+
+
+
+    private fun getZoom(methodCall: MethodCall, result: MethodChannel.Result) {
+            livemap.getZoom(GetZoomCallback{zoomLevel->
+                result.success(zoomLevel);
+            })
+        }
+
     override fun dispose() {
         channel.setMethodCallHandler(null)
     }
+
+
+
 }
--- lib/livemap_controller.dart
+++ lib/livemap_controller.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/services.dart';
 import 'package:maplibre_gl_platform_interface/maplibre_gl_platform_interface.dart';
 
@@ -9,6 +12,13 @@ typedef void OnIndoorFeatureClickCallback(dynamic data);
 typedef void OnIndoorLevelChangedCallback(dynamic data);
 typedef void OnIndoorLevelsChangedCallback(List<dynamic> data);
 typedef void OnMapClickCallback(dynamic coordinates);
+// typedef void OnContentUpdatedCallback(List<dynamic> pinpoints);
+typedef void OnPinpointUpdatedCallback(List<dynamic> pinpoints);
+typedef void OnEventUpdatedCallback(List<dynamic> events);
+
+typedef void OnUserLoginCallback();
+typedef GetZoomCallback = void Function(dynamic zoomLevel);
+typedef FindNearestPinpointsCallback = void Function(String pinpoints);
 
 class LivemapController {
   late MethodChannel _channel;
@@ -21,6 +31,10 @@ class LivemapController {
   final OnIndoorLevelChangedCallback? onIndoorLevelChanged;
   final OnIndoorLevelsChangedCallback? onIndoorLevelsChanged;
   final OnMapClickCallback? onMapClick;
+  final OnPinpointUpdatedCallback? onPinpointUpdated;
+  final OnEventUpdatedCallback? onEventUpdated;
+
+  final OnUserLoginCallback? onUserLogin;
 
   final onMapReadyPlatform = ArgumentCallbacks<void>();
   final onPinpointOpenPlatform = ArgumentCallbacks<dynamic>();
@@ -31,6 +45,10 @@ class LivemapController {
   final onIndoorLevelChangedPlatform = ArgumentCallbacks<dynamic>();
   final onIndoorLevelsChangedPlatform = ArgumentCallbacks<List<dynamic>>();
   final onMapClickPlatform = ArgumentCallbacks<dynamic>();
+  final onPinpointUpdatedPlatform = ArgumentCallbacks<List<dynamic>>();
+  final onEventUpdatedPlatform = ArgumentCallbacks<List<dynamic>>();
+
+  final onUserLoginPlatform = ArgumentCallbacks<void>();
 
   LivemapController(int id,
       {this.onMapReady,
@@ -41,7 +59,11 @@ class LivemapController {
       //this.onFloorChanged,
       this.onIndoorLevelChanged,
       this.onIndoorLevelsChanged,
-      this.onMapClick}) {
+      this.onMapClick,
+      // this.onContentUpdated,
+      this.onPinpointUpdated,
+      this.onEventUpdated,
+      this.onUserLogin}) {
     _channel = MethodChannel('MapView/$id');
     _channel.setMethodCallHandler(_handleMethod);
 
@@ -76,9 +98,21 @@ class LivemapController {
     //   }
     // });
 
-    onContentUpdatedPlatform.add((List<dynamic> pinpoints) {
-      if (onContentUpdated != null) {
-        onContentUpdated!(pinpoints);
+    onPinpointUpdatedPlatform.add((List<dynamic> pinpoints) {
+      if (onPinpointUpdated != null) {
+        onPinpointUpdated!(pinpoints);
+      }
+    });
+
+    onEventUpdatedPlatform.add((List<dynamic> events) {
+      if (onEventUpdated != null) {
+        onEventUpdated!(events);
+      }
+    });
+
+    onUserLoginPlatform.add((_) {
+      if (onUserLogin != null) {
+        onUserLogin!();
       }
     });
 
@@ -144,9 +178,17 @@ class LivemapController {
         dynamic data = call.arguments as dynamic;
         onIndoorLevelsChangedPlatform(data);
         break;
-      case 'onContentUpdated':
+      case 'onPinpointUpdated':
         List<dynamic> pinpoints = call.arguments as List<dynamic>;
-        onContentUpdatedPlatform(pinpoints);
+        onPinpointUpdatedPlatform(pinpoints);
+        break;
+      case 'onEventUpdated':
+        List<dynamic> pinpoints = call.arguments as List<dynamic>;
+        onEventUpdatedPlatform(pinpoints);
+        break;
+
+      case 'onUserLogin':
+        onUserLoginPlatform(null);
         break;
     }
   }
@@ -160,7 +202,6 @@ class LivemapController {
       print("Error from native: $e.message");
     }
   }
-
 
   Future<void> openPinpoint(int pinpointId,
       {Map<String, dynamic>? options}) async {
@@ -209,4 +250,117 @@ class LivemapController {
     _channel.invokeMethod('setIndoorFeatureState', {'id': id, 'state': state});
   }
 
+  Future<void> openEvent(int eventId) async {
+    await _channel.invokeMethod('openEvent', {"event": eventId});
+  }
+
+  Future<void> closeEvent() async {
+    await _channel.invokeMethod('closeEvent');
+  }
+
+  Future<void> openList(int listId) async {
+    await _channel.invokeMethod('openList', {"list": listId});
+  }
+
+  Future<void> closeList() async {
+    await _channel.invokeMethod('closeList');
+  }
+
+  Future<void> closePopin() async {
+    await _channel.invokeMethod('closePopin');
+  }
+
+  Future<void> setFilters({required Map<String, dynamic> filters}) async {
+    await _channel.invokeMethod('setFilters', {"filters": filters});
+  }
+
+  Future<void> navigateToPinpoint(int pinpointId) async {
+    await _channel.invokeMethod('navigateToPinpoint', {"pinpoint": pinpointId});
+  }
+
+  Future<void> stopNavigation() async {
+    await _channel.invokeMethod('stopNavigation');
+  }
+
+  Future<void> signInByToken({required String accessToken}) async {
+    await _channel.invokeMethod('signInByToken', {"accessToken": accessToken});
+  }
+
+  Future<void> enableSidebar() async {
+    await _channel.invokeMethod('enableSidebar');
+  }
+
+  Future<void> disableSidebar() async {
+    await _channel.invokeMethod('disableSidebar');
+  }
+
+  Future<void> signOut() async {
+    await _channel.invokeMethod('signOut');
+  }
+
+  Future<void> setSourceLists({required List<int> sourceLists}) async {
+    await _channel.invokeMethod('setSourceLists', {"sourceLists": sourceLists});
+  }
+
+  Future<void> setPinpoints(
+      {required List<Map<String, dynamic>> pinpoints}) async {
+    await _channel.invokeMethod('setPinpoints', {"pinpoints": pinpoints});
+  }
+
+  Future<void> setEvents({required List<Map<String, dynamic>> events}) async {
+    await _channel.invokeMethod('setEvents', {"events": events});
+  }
+
+  Future<void> aroundMe() async {
+    await _channel.invokeMethod('aroundMe');
+  }
+
+  Future<void> enableAnalytics() async {
+    await _channel.invokeMethod('enableAnalytics');
+  }
+
+  Future<void> disableAnalytics() async {
+    await _channel.invokeMethod('disableAnalytics');
+  }
+
+  Future<void> drawPolyline(
+      {required List<Map<String, dynamic>> coordinates}) async {
+    await _channel.invokeMethod('drawPolyline', {"coordinates": coordinates});
+  }
+
+  Future<void> removePolyline({required String polylineId}) async {
+    await _channel.invokeMethod('removePolyline', {"polylineId": polylineId});
+  }
+
+  Future<void> addmarker({required Map<String, dynamic> marker}) async {
+    await _channel.invokeMethod('addmarker', {"marker": marker});
+  }
+
+  Future<void> removeMarker({required String markerId}) async {
+    await _channel.invokeMethod('removeMarker', {"markerId": markerId});
+  }
+
+  Future<void> findNearestPinpoints(
+      {required Map<String, dynamic> center,
+      required FindNearestPinpointsCallback
+          findNearestPinpointsCallback}) async {
+    try {
+      final String result = await _channel
+          .invokeMethod('findNearestPinpoints', {"center": center});
+      findNearestPinpointsCallback(result);
+      print("Result(findNearestPinpoints) from native: $result");
+    } on PlatformException catch (e) {
+      print("Error from native: $e.message");
+    }
+  }
+
+  Future<void> getZoom({required GetZoomCallback getZoomCallback}) async {
+    try {
+      final double result = await _channel.invokeMethod('getZoom');
+      getZoomCallback(result);
+      print("Result from native: $result");
+    } on PlatformException catch (e) {
+      print("Error from native: $e.message");
+    }
+  }
 }
